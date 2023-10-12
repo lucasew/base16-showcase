a3cc4f7adc22fb1ef51ea1572a24500fdcc661c9
lucasew <lucas59356@gmail.com>
2023-10-12 10:28:55 -0300

    i18n: a little more inteligent handling
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,39 +1,39 @@
 const i18n = {
   loading: {
-    en_US: "Loading...",
-    pt_BR: "Carregando...",
+    en: "Loading...",
+    pt: "Carregando...",
   },
   error: {
-    en_US: "Error",
-    pt_BR: "Erro",
+    en: "Error",
+    pt: "Erro",
   },
   load: {
-    en_US: "Load",
-    pt_BR: "Carregar",
+    en: "Load",
+    pt: "Carregar",
   },
   no_themes_loaded: {
-    en_US: "No themes loaded",
-    pt_BR: "Nenhum tema carregado"
+    en: "No themes loaded",
+    pt: "Nenhum tema carregado"
   },
   title: {
-     en_US: "Base16 theme showcase",
-     pt_BR: "Demonstração de temas Base16"
+     en: "Base16 theme showcase",
+     pt: "Demonstração de temas Base16"
   },
   instruction_supported_formats: {
-    en_US: "You can load a nix-colors structured JSON or a base16 YAML theme file.",
-    pt_BR: "Você pode carregar um JSON estruturado de acordo com o nix-colors ou um tema YAML no formato do base16"
+    en: "You can load a nix-colors structured JSON or a base16 YAML theme file.",
+    pt: "Você pode carregar um JSON estruturado de acordo com o nix-colors ou um tema YAML no formato do base16"
   },
   instruction_other_details: {
-    en_US: "Some other details may be covered by this app.",
-    pt_BR: "Alguns outros detalhes podem ser cobertos por este app."
+    en: "Some other details may be covered by this app.",
+    pt: "Alguns outros detalhes podem ser cobertos por este app."
   },
   instruction_ingestion: {
-    en_US: "You can drag-and-drop the files or double-click anywhere in the app to open a file selector.",
-    pt_BR: "Você pode arrastar e soltar os arquivos ou dar um duplo clique em qualquer lugar do app para abrir um seletor de arquivos."
+    en: "You can drag-and-drop the files or double-click anywhere in the app to open a file selector.",
+    pt: "Você pode arrastar e soltar os arquivos ou dar um duplo clique em qualquer lugar do app para abrir um seletor de arquivos."
   },
   instruction_load_default_colors: {
-    en_US: "You can also load the default pre-defined colorscheme pack.",
-    pt_BR: "Você também pode carregar o conjunto pré-definido de esquemas de cores."
+    en: "You can also load the default pre-defined colorscheme pack.",
+    pt: "Você também pode carregar o conjunto pré-definido de esquemas de cores."
   }
 };
 
@@ -41,16 +41,14 @@ export type i18nKeys = keyof typeof i18n
 
 export type i18Led = Record<string, string> | string;
 
-const DEFAULT_LOCALE = 'en_US'
+const DEFAULT_LOCALE = 'en'
 
 export function i18nGet(txt: i18Led) {
-  const locale =
+  let locale =
     navigator.language || (navigator as any).userLanguage || "en_US";
-  const string = txt[locale.replaceAll("-", "_")]
-  if (!string) {
-      return txt[DEFAULT_LOCALE]
-  }
-  return string
+
+  locale = locale.replaceAll("-", "_")
+  return txt[locale] || txt[locale.split('_')[0]] || txt[DEFAULT_LOCALE]
 }
 
 export function i18nString(label: i18nKeys) {
