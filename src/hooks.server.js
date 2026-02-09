dc8058c023770e3354c9ab9ea909d0728966847d
lucasew <lucas59356@gmail.com>
2026-02-09 09:17:10 -0300

    sentry
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,11 +1,14 @@
+import {sequence} from '@sveltejs/kit/hooks';
+import * as Sentry from '@sentry/sveltekit';
 // src/hooks.server.js
 import { paraglideMiddleware } from '$lib/paraglide/server.js';
 
 /** @type {import('@sveltejs/kit').Handle} */
-export const handle = ({ event, resolve }) =>
+export const handle = sequence(Sentry.sentryHandle(), ({ event, resolve }) =>
 	paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
 		event.request = localizedRequest;
 		return resolve(event, {
 			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
 		});
-	});
+	}));
+export const handleError = Sentry.handleErrorWithSentry();
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		paraglideVitePlugin({
-			project: './project.inlang',
-			outdir: './src/lib/paraglide',
-			strategy: ['preferredLanguage', 'baseLocale']
-		}),
-		sveltekit()
-	]
+	plugins: [sentrySvelteKit({
+        org: "lucao-enterprise",
+        project: "base16-showcase"
+    }), tailwindcss(), paraglideVitePlugin({
+        project: './project.inlang',
+        outdir: './src/lib/paraglide',
+        strategy: ['preferredLanguage', 'baseLocale']
+    }), sveltekit()]
 });
